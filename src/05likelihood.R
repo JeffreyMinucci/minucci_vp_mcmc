af8817e6d8da0c33ad6373879bd179c5a24655e6
Jeff Minucci <jminucci@uga.edu>
2018-02-01 11:28:59 -0500

    Add likelihood calculation

---
+++ src/05likelihood.R
@@ -0,0 +1,24 @@
+##
+#  Code to calculate likelihood for a varroapop run given field data on colony population 
+#
+#  Author: Jeffrey Minucci
+#
+#
+##
+
+#Create hypothetical field data (replace with real data when available)
+adult_pop_month1 <- floor(rnorm(10,4500,1000))
+
+#Model prediction - note this is total colony size
+pred <- tdarray_control[30,1,1]
+var_est <- var(adult_pop_month1) #for now get var from actual data
+
+
+vp_loglik_simple <- function(actual,pred,var){
+  ll <- (length(actual)/-2)*log(2*pi) - (length(actual)/2)*log(var)- (1/(2*var)) * sum((actual - pred)^2)
+  
+}
+
+
+#test log likelihood function
+like <- vp_loglik_simple(adult_pop_month1,pred,var_est)
