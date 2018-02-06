a91c6b50de889222d529fc2c5ba1d96c4cec365e
Jeff Minucci <jminucci@uga.edu>
2018-02-06 15:34:30 -0500

    optimize read results wrapper file

--- src/04read_output.R
+++ src/04read_output.R
@@ -1,29 +1,31 @@
 # load up varroapop data into a 3d array dataframe ######
 
-df <- read.table(paste(vpdir_out_control,"results",i,".txt", sep=""), header= FALSE, sep= "", 
-                skip = 6, stringsAsFactors = FALSE, row.names=NULL)
-dim(df)
-nrows <- dim(df)[[1]] #this is dependent on the duration of the simulation as set in the comparison.vrp file
-ncols <- dim(df)[[2]]
 
-timearray<- array(data=NA, c(nrows))
-timearray[2:nrows]<- df[2:nrows,1]
-timearray<- as.Date(timearray,"%m/%d/%Y")
-timediff <- timearray[3]-timearray[2]
-timearray[1] <- timearray[2]-timediff
-length(timearray)
-ncols
-outvar<- c("Date","Colony Size","Adult Drones","Adult Workers", "Foragers", "Capped Drone Brood", "Capped Worker Brood",
-           "Drone Larvae", "Worker Larvae", "Drone Eggs", "Worker Eggs", "Free Mites", "Drone Brood Mites",
-           "Worker Brood Mites", "Mites/Drone Cell", "Mites/Worker Cell", "Mites Dying", "Proportion Mites Dying",
-           "Colony Pollen (g)", "Pollen Pesticide Concentration", "Colony Nectar", "Nectar Pesticide Concentration",
-           "Dead Drone Larvae", "Dead Worker Larvae", "Dead Drone Adults", "Dead Worker Adults", "Dead Foragers",
-           "Queen Strength", "Average Temperature (celsius)", "Rain")
-length(outvar)
 
+if(i==1){
+  df <- read.table(paste(vpdir_out_control,"results",i,".txt", sep=""), header= FALSE, sep= "", 
+                   skip = 6, stringsAsFactors = FALSE, row.names=NULL)
+  dim(df)
+  nrows <- dim(df)[[1]] #this is dependent on the duration of the simulation as set in the comparison.vrp file
+  ncols <- dim(df)[[2]]
+  
+  timearray<- array(data=NA, c(nrows))
+  timearray[2:nrows]<- df[2:nrows,1]
+  timearray<- as.Date(timearray,"%m/%d/%Y")
+  timediff <- timearray[3]-timearray[2]
+  timearray[1] <- timearray[2]-timediff
+  length(timearray)
+  ncols
+  outvar<- c("Date","Colony Size","Adult Drones","Adult Workers", "Foragers", "Capped Drone Brood", "Capped Worker Brood",
+             "Drone Larvae", "Worker Larvae", "Drone Eggs", "Worker Eggs", "Free Mites", "Drone Brood Mites",
+             "Worker Brood Mites", "Mites/Drone Cell", "Mites/Worker Cell", "Mites Dying", "Proportion Mites Dying",
+             "Colony Pollen (g)", "Pollen Pesticide Concentration", "Colony Nectar", "Nectar Pesticide Concentration",
+             "Dead Drone Larvae", "Dead Worker Larvae", "Dead Drone Adults", "Dead Worker Adults", "Dead Foragers",
+             "Queen Strength", "Average Temperature (celsius)", "Rain")
+  length(outvar)
+}
 
 # read output files
-#CONTROL
 
 tdarray_control <- array(data=NA, c(nrows,ncols-1,Nsims))
 dim(tdarray_control)
