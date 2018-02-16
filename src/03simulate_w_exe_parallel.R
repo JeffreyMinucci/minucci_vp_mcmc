2824e275ad9ac9f9ce2367e0873489fa92662b8f
Jeff Minucci <jminucci@uga.edu>
2018-02-16 16:16:32 -0500

    update function documentation

--- src/03simulate_w_exe_parallel.R
+++ src/03simulate_w_exe_parallel.R
@@ -1,5 +1,18 @@
-#Runs 10 sims of VP in parallel - one for each site, which is a unique combination of starting 
+#Wrapper to run 10 sims of VP in parallel - one for each site, which is a unique combination of starting 
 #parameters and neonic exposure
+# 
+# inputs:
+#
+# i = iteration number. For determing what to name results and logs. Useful if calling this function inside of a loop.
+# exe_path = path to the folder which contains the .exe (e.g. 'D:/minucci_vp_mcmc/bin/)
+# exe_name = full path to the .exe (e.g. 'D:/minucci_vp_mcmc/bin/VarroaPop.exe')
+# vrp_name = name of the .vrp file (e.g. 'default_jeff.vrp')
+# input_name = name of the input file (e.g. 'input_mcmc.txt')
+# in_path = path to the input folder e.g. 'D:/minucci_vp_mcmc/input/control/
+# out_path = path to the output folder e.g. 'D:/minucci_vp_mcmc/output/control/
+# log_path = path to the log folder e.g. 'D:/minucci_vp_mcmc/log/control/ (can be omitted if logs=F)
+# logs = T/F, should VP write a log file to the log_path folder?
+
 
 run_vp_parallel <- function(i,exe_path, exe_name, vrp_name, in_path, out_path,log_path=NULL,logs=T,debug=F){
   if(debug) print(paste("Number of cores utilized:",getDoParWorkers()))
