747a8c0b14a9abe210e12b59c52a2a96e16699ac
Jeff Minucci <jminucci@uga.edu>
2019-02-14 10:37:46 -0500

    remove print statements

--- tools/DataSimulator.py
+++ tools/DataSimulator.py
@@ -3,26 +3,24 @@ from itertools import product
 import numpy as np
 import os
 import pandas as pd
+from math import floor
 
 #DEBUG options
 LOGS = False
 
-#Start date for sims = CCA3, 06/20/2014
+#Start date for sims
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..','data'))
-INITIAL_DF = os.path.join(DATA_DIR, 'initial_conditions.csv')
-START_DATE = '06/20/2014'
-END_DATE = '10/22/2014'
+INITIAL_DF = os.path.join(DATA_DIR, 'raw/','field_initial_conditions.csv')
+START_DATES = ['04/29/2015','04/27/2015', '04/27/2015', '04/28/2015', '04/28/2015', '05/01/2015', '04/30/2015',
+               '04/29/2015', '04/28/2015', '04/30/2015']
+END_DATE = '08/25/2015'
 
-DATES = ['4', '5', '6', '7']
-DATES_STR = ['07/16/2014', '08/08/2014','09/10/2014', '10/15/2014']
-DATES_STR_HIGH = ['07/16/2014', '08/08/2014','09/10/2014', '10/21/2014'] #high had a late CCA7
-TREATMENTS = ['0', '10', '20', '40', '80', '160']
-REPS = [24, 12, 12, 12, 12, 12]
-#REPS = [3,3,3,3,3,3] #for testing
-#REPS = [1,1,1,1,1,1] #for testing
+DATES = ['5', '6', '8']
+DATES_STR = pd.read_csv(os.path.join(DATA_DIR+"/raw/field_bee_areas.csv")).iloc[:, 15:19]
+SITES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 RESPONSE_VARS = [('Adults', ['Adult Drones', 'Adult Workers', 'Foragers']),('Pupae',['Capped Drone Brood', 'Capped Worker Brood']),
                  ('Larvae', ['Drone Larvae', 'Worker Larvae']),  ('Eggs', ['Drone Eggs', 'Worker Eggs'])]
-RESPONSE_FILTER = ['Adults_mean', 'Adults_sd'] #For now use only these responses!
+RESPONSE_FILTER = ['Adults'] #For now use only these responses!
 
 
 def simulate(pars, save = False, logs = False):
@@ -38,61 +36,48 @@ def simulate(pars, save = False, logs = False):
                 AIAdultSlope
                 AILarvaLD50   # in log10!
                 AILarvaSlope
+                AIHalfLife
     :return a dictionary of summary stats
     """
     #print(DATA_DIR)
     #print(INITIAL_DF)
     #print(parameters)
     parameters = pars.copy() #copy our inputs so that we don't ever modify them (pyabc needs these)
-    static_pars = {'SimStart': START_DATE, 'SimEnd': END_DATE, 'IPollenTrips': 8, 'INectarTrips': 17,
-                   'AIHalfLife': 25, 'RQEnableReQueen': 'false'}
+    static_pars = {'SimEnd': END_DATE, 'IPollenTrips': 8, 'INectarTrips': 17, 'RQEnableReQueen': 'false'}
     for name, value in parameters.items():
         if not name.endswith(('_mean','_sd')):
             static_pars[name] = value
     static_pars['AIAdultLD50'] = 10**static_pars['AIAdultLD50'] #un log transform
     static_pars['AILarvaLD50'] = 10**static_pars['AILarvaLD50'] #un log transform
     static_pars['NecPolFileEnable'] = 'true'
-    weather_path = os.path.join(DATA_DIR,'15055_grid_35.875_lat.wea')# os.path.abspath(os.path.join('data', '15055_grid_35.875_lat.wea'))
+    weather_path = os.path.join(DATA_DIR,'external/weather/weather_2015','18815_grid_39.875_lat.wea')
     #all_responses = pd.DataFrame(index = rows, columns = cols)
     all_responses = pd.DataFrame()
-    for index, trt in enumerate(TREATMENTS):
-        trt_responses_mean = np.empty((len(DATES), len(RESPONSE_VARS)))
-        trt_responses_sd = np.empty((len(DATES), len(RESPONSE_VARS)))
-        trt_pars = static_pars.copy()
-        exposure_filename = 'clo_feeding_' + trt + '.csv'
-        exposure_path = os.path.join(DATA_DIR, exposure_filename)#os.path.abspath(os.path.join('data', exposure_filename))
-        trt_pars['NecPolFileName'] = exposure_path
-        reps = REPS[index]
-        rep_responses = np.empty(([len(DATES),len(RESPONSE_VARS),reps])) #survey dates (rows) x output vars (cols) x reps (z axis)
-        for rep in range(0,reps):
-            vp_pars = generate_start(trt_pars.copy(), trt)
-            #generate random gaussian parameters
-            vp_pars['ICQueenStrength'] = 0
-            vp_pars['ICForagerLifespan'] = 0
-            while not (1 <=vp_pars['ICQueenStrength'] <= 5):
-                vp_pars['ICQueenStrength'] = np.random.normal(loc = float(parameters['ICQueenStrength_mean']), scale = float(parameters['ICQueenStrength_sd']))
-            while not (4 <= vp_pars['ICForagerLifespan'] <= 16):
-                vp_pars['ICForagerLifespan'] = np.random.normal(loc = float(parameters['ICForagerLifespan_mean']), scale = float(parameters['ICForagerLifespan_sd']))
-            vp = VarroaPop(parameters = vp_pars, weather_file = weather_path, verbose=False, unique=True, keep_files=save, logs=logs)
-            vp.run_model()
-            if trt == "160":
-                dates = DATES_STR_HIGH
-            else:
-                dates = DATES_STR
-            rep_responses[:,:,rep] = filter_rep_responses(vp.get_output(), dates_str= dates)
-        mean_cols = [var[0]+"_mean" for var in RESPONSE_VARS]
-        sd_cols = [var[0]+"_sd" for var in RESPONSE_VARS]
-        trt_responses_mean = pd.DataFrame(np.mean(rep_responses,axis=2), columns = mean_cols)
-        trt_responses_sd = pd.DataFrame(np.std(rep_responses,axis=2, ddof=1), columns = sd_cols) # Note: uses sample sd, not population sd
-        trt_responses = pd.concat([trt_responses_mean, trt_responses_sd], axis = 1)
-        #print("Treatment {} responses: {}".format(trt,trt_responses))
+    for index, site in enumerate(SITES):
+        #site_response = np.empty((len(DATES), len(RESPONSE_VARS)))
+        exposure_filename = 'neonic_profile_' + site + '.csv'
+        exposure_path = os.path.join(DATA_DIR,"processed/neonic_profiles/", exposure_filename)#os.path.abspath(os.path.join('data', exposure_filename))
+        site_pars = generate_start(static_pars.copy(), site)
+        site_pars['NecPolFileName'] = exposure_path
+        site_pars['ICQueenStrength'] = 0
+        site_pars['ICForagerLifespan'] = 0
+        while not (1 <=site_pars['ICQueenStrength'] <= 5):
+            site_pars['ICQueenStrength'] = np.random.normal(loc = float(parameters['ICQueenStrength_mean']), scale = float(parameters['ICQueenStrength_sd']))
+        while not (4 <= site_pars['ICForagerLifespan'] <= 16):
+            site_pars['ICForagerLifespan'] = np.random.normal(loc = float(parameters['ICForagerLifespan_mean']), scale = float(parameters['ICForagerLifespan_sd']))
+        vp = VarroaPop(parameters = site_pars, weather_file = weather_path, verbose=False, unique=True, keep_files=save, logs=logs)
+        vp.run_model()
+        dates = DATES_STR.iloc[index,:]
+        site_response = filter_site_responses(vp.get_output(), dates_str= dates)
+        #print("Site {} responses: {}".format(site, site_response))
         start_row = len(DATES)*index
         end_row = start_row + len(DATES)
-        #all_responses.loc[start_row:end_row,:] = trt_responses
-        all_responses = all_responses.append(trt_responses, ignore_index=True)
+        #all_responses.loc[start_row:end_row,:] = site_responses
+        #print("Site {}: {}".format(index,site_response))
+        all_responses = all_responses.append(site_response, ignore_index=True)
 
     #Generate labels for rows and columns
-    rows = ['_'.join(x) for x in product(TREATMENTS, DATES)]
+    rows = ['_'.join(x) for x in product(SITES, DATES)]
     #print('Row labels: {}'.format(rows))
     response_cols = [x[0] for x in RESPONSE_VARS]
     cols = ['_'.join([x,y]) for y in ['mean', 'sd'] for x in response_cols]
@@ -112,7 +97,7 @@ def simulate(pars, save = False, logs = False):
     return output_dict
 
 
-def filter_rep_responses(output, dates_str):
+def filter_site_responses(output, dates_str):
     """
     From raw VarroaPop output, filter out the dates and response variables that we need for our summary stats.
     :param output: dataframe of VarroaPop raw outputs
@@ -127,30 +112,31 @@ def filter_rep_responses(output, dates_str):
     #print(responses)
     col_names = [x[0] for x in RESPONSE_VARS]
     response_df = pd.DataFrame.from_items(zip(col_names,responses))
+    #print(response_df)
     #print("REP filtered responses: {}".format(response_df))
     return response_df
 
 
-def generate_start(paras, trt):
+def generate_start(pars, site):
+    paras = pars.copy()
     df = pd.read_csv(INITIAL_DF)
-    df['treatment'] = df['treatment'].astype('str', copy=True)
-    df.set_index('treatment', inplace=True)
+    df['site'] = np.arange(1,11,1).astype("str")
+    df.set_index('site', inplace=True)
     #print("Initial conditions: {}".format(df))
-    vars = ['adult', 'pupae', 'larvae', 'eggs', 'pollen', 'honey'] #numbers to generate
-    vals = [-1,-1,-1,-1,-1,-1]
-    for i, var in enumerate(vars):
-        while vals[i] < 0:
-            vals[i] = np.random.normal(loc = df.loc[trt,var+'_mean'], scale = df.loc[trt,var+'_sd'])
-    paras['ICWorkerAdults'] = vals[0]
+    paras["SimStart"] = START_DATES[int(site)-1] #convert site number to 0-9 list index
+    vars = ['bees_cm2_4', 'capped_cm2_4', 'open_cm2_4', 'pollen_cm2_4', 'nectar_cm2_4'] #cols of initial conditions
+    vals = df.loc[site,vars].copy()
+    cells_per_cm2 = 3.96 #based on hex w flat to flat distance of 0.54 cm = cell vol of 0.2525 cm2
+    paras['ICWorkerAdults'] = floor(vals[0] * 1.45) #number from Chia
     paras['ICDroneAdults'] = 0
-    paras['ICWorkerBrood'] = vals[1]
+    paras['ICWorkerBrood'] = floor(vals[1] * cells_per_cm2 * .66) #based on time in stage
     paras['ICDroneBrood'] = 0
-    paras['ICWorkerLarvae'] = vals[2]
+    paras['ICWorkerLarvae'] = floor(vals[1] * cells_per_cm2 * .33) #based on time in stage
     paras['ICDroneLarvae'] = 0
-    paras['ICWorkerEggs'] = vals[3]
+    paras['ICWorkerEggs'] = floor(vals[2] * cells_per_cm2)
     paras['ICDroneEggs'] = 0
-    paras['InitColPollen'] = vals[4] * 0.425 #based off 0.293 cm3 cell volume and 1.45 g/cm3 pollen density
-    paras['InitColNectar'] = vals[5] * 0.331 #based off 0.293 cm3 cell volume and 1.13 g/cm3 nectar density
+    paras['InitColPollen'] = floor(vals[3] * cells_per_cm2* 0.316 *1.45) #based off 0.316 cm3 cell volume and 1.45 g/cm3 pollen density
+    paras['InitColNectar'] = floor(vals[4] * cells_per_cm2* 0.316 * 1.13) #based off 0.316 cm3 cell volume and 1.13 g/cm3 nectar density
     return paras
 
 
